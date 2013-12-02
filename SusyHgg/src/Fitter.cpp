51324ec6b000d5a1a3364246a0523b11cd6c1180
Alex <armott@gmail.com>
2013-12-02 13:02:51 -0800

    defined more correct signal regions

--- SusyHgg/src/Fitter.cpp
+++ SusyHgg/src/Fitter.cpp
@@ -18,6 +18,9 @@ Fitter::Fitter(TString inputFileName,TString outputFileName) {
 
   //
   defineCats();
+  setupCats();
+
+  lumi = ws->var("N_data")->getVal();
 }
 
 Fitter::~Fitter() {
@@ -30,38 +33,60 @@ Fitter::~Fitter() {
 void Fitter::Run() {
   for(int catIndex=0;catIndex < cats.size();catIndex++) {
     buildHistograms(catIndex);
-    runFits(catIndex);
+    /*
+      runFits(catIndex);
     computeScaleFactor(catIndex);
     buildSubtractedHistograms(catIndex);
+    */
   }
+  for(auto& it: save_histograms) {
+    
+    TH2F* tmp = getSignalRegionHistogram(*it.second,it.first+"_SigRegions");
+    save_SigRegion_histograms[tmp->GetName()] = tmp;
+  }
+  SMFit();
+
+  for(auto& it: sm_higgs_names) {
+    ws->import(*buildSimultaneousPdf(it+"_%s_Signal_SigRegions_pdf"));
+  }
+  if(sm_higgs_names.size()>0) ws->import(*buildSimultaneousPdf("SMTot_%s_Signal_SigRegions_pdf"));
+
+  for(auto& it: sms_names) {
+    ws->import(*buildSimultaneousPdf(it+"_%s_Signal_SigRegions_pdf"));
+  }
+
   saveAll();
 }
 
 void Fitter::buildHistograms(int catIndex) {
   std::cout << "buildHistograms " << catIndex << std::endl;
 
-  save_histograms[Form("data_%s_Signal",cats.at(catIndex).Data())] = build_histogram("data",catIndex,kSignal);
-  save_histograms[Form("data_%s_Sideband",cats.at(catIndex).Data())] = build_histogram("data",catIndex,kSideband);
-  save_histograms[Form("data_%s_All",cats.at(catIndex).Data())] = build_histogram("data",catIndex,kAll);
-  
+  if(processData) {
+    save_histograms[Form("data_%s_Signal",cats.at(catIndex).Data())] = build_histogram("data",catIndex,kSignal);
+    save_histograms[Form("data_%s_Sideband",cats.at(catIndex).Data())] = build_histogram("data",catIndex,kSideband);
+    save_histograms[Form("data_%s_All",cats.at(catIndex).Data())] = build_histogram("data",catIndex,kAll);
+  }
   TH2F* SMTot=0;
   for(auto& mc_name : sm_higgs_names) {
     RooRealVar *n = ws->var("N_"+mc_name);
-    save_histograms[Form("%s_%s_Signal",mc_name.Data(),cats.at(catIndex).Data())] = build_histogram(mc_name,catIndex,kSignal,xsecs[mc_name] * 2.28E-3 * 19780/n->getVal());
+    save_histograms[Form("%s_%s_Signal",mc_name.Data(),cats.at(catIndex).Data())] = build_histogram(mc_name,catIndex,kSignal,xsecs[mc_name] * 2.28E-3 * lumi*1000/n->getVal());
     if(SMTot==0) SMTot = (TH2F*) save_histograms[Form("%s_%s_Signal",mc_name.Data(),cats.at(catIndex).Data())]->Clone("SMTot_"+cats.at(catIndex)+"_Signal");
     else SMTot->Add(save_histograms[Form("%s_%s_Signal",mc_name.Data(),cats.at(catIndex).Data())]);
   }
-  save_histograms[Form("SMTot_%s_Signal",cats.at(catIndex).Data())] = SMTot;
-  
+  if(SMTot) {
+    save_histograms[Form("SMTot_%s_Signal",cats.at(catIndex).Data())] = SMTot;
+  }
+
   for(auto& mc_name : sms_names) {
     RooRealVar *n = ws->var("N_"+mc_name);
-    save_histograms[Form("%s_%s_Signal",mc_name.Data(),cats.at(catIndex).Data())] = build_histogram(mc_name,catIndex,kSignal,1.0 * 2.28E-3 * 19780/n->getVal());      
+    save_histograms[Form("%s_%s_Signal",mc_name.Data(),cats.at(catIndex).Data())] = build_histogram(mc_name,catIndex,kSignal,1.0 * 2.28E-3 * lumi * 1000/n->getVal());      
   }
   
  }
 
 
 void Fitter::runFits(int catIndex) {
+  if(!processData) return;
   std::cout << "runFits " << catIndex << std::endl;
 
   RooRealVar *mgg = ws->var("mgg");
@@ -92,6 +117,7 @@ void Fitter::runFits(int catIndex) {
 }
 
 void Fitter::computeScaleFactor(int catIndex) {
+  if(!processData) return;
   std::cout << "computeScaleFactor " << catIndex << std::endl;
   TString tag = cats.at(catIndex);
   RooRealVar *a1 = ws->var( Form("%s_alpha1",tag.Data()) );
@@ -158,6 +184,7 @@ void Fitter::computeScaleFactor(int catIndex) {
 }
 
 void Fitter::buildSubtractedHistograms(int catIndex) {
+  if(!processData) return;
   std::cout << "buildSubtractedHistogram " << catIndex << std::endl;
   TH2F* signal   = save_histograms[ "data_"+cats.at(catIndex)+"_Signal"];
   TH2F* sideband = save_histograms[ "data_"+cats.at(catIndex)+"_Sideband"];
@@ -173,6 +200,45 @@ void Fitter::buildSubtractedHistograms(int catIndex) {
   }
 
   save_histograms["data_"+cats.at(catIndex)+"_Signal_sidebandSub"] = bkg_sub;
+}
+
+void Fitter::SMFit() {
+  if(!processData) return;
+  std::map<std::string,RooDataHist*> hists;
+  RooSimultaneous sideband("data_Sideband_SigRegions_norm_pdf","data_Sideband_SigRegions_norm_pdf",*roocat);
+  RooSimultaneous SMTot("SMTot_Signal_SigRegions_norm_pdf","SMTot_Signal_SigRegions_norm_pdf",*roocat);
+  
+  for(int catIndex=0;catIndex < cats.size();catIndex++) {
+    std::string name = cats.at(catIndex).Data();
+
+    RooDataHist *data =  (RooDataHist*)ws->data( Form("data_%s_Signal_SigRegions_norm_hist",name.c_str()) );
+    assert(data != 0 && Form("data named data_%s_Signal_SigRegions_norm_hist NOT FOUND",name.c_str()));
+    hists[name] = data;
+
+    RooAbsPdf *side = ws->pdf( Form("data_%s_Sideband_SigRegions_norm_pdf",name.c_str()) );
+    sideband.addPdf(*side,name.c_str());
+
+    RooAbsPdf *sm = ws->pdf( Form("SMTot_%s_Signal_SigRegions_norm_pdf",name.c_str()) );
+    SMTot.addPdf(*sm,name.c_str());
+  }
+  //build the multicategory data set
+  RooRealVar *sig_region = ws->var("signal_region");
+
+  RooDataHist data_tot("data_Signal_SigRegions_norm_hist","",*sig_region,*roocat,hists);
+
+  RooRealVar NSM("N_FITTED_SMTot","N_FITTED_SMTot",100,0,1e7);
+  RooRealVar NSide("N_FITTED_Sideband","N_FITTED_Sideband",10000,0,1e9);
+  
+  RooAddPdf simultaneous_sum("data_plus_SMTot_Sideband_SigRegions_norm_pdf","",RooArgList(sideband,SMTot),RooArgList(NSide,NSM));
+
+  simultaneous_sum.fitTo(data_tot,RooFit::Save(kFALSE),RooFit::Strategy(0),RooFit::NumCPU(NUM_CPU),RooFit::Extended(kTRUE));
+  RooFitResult* res = simultaneous_sum.fitTo(data_tot,RooFit::Save(kTRUE),RooFit::Strategy(2),RooFit::NumCPU(NUM_CPU),RooFit::Extended(kTRUE));
+  res->SetName( "data_plus_SMTot_Sideband_SigRegions_norm_FITRESULT" );
+
+  ws->import(simultaneous_sum);
+  ws->import(data_tot);
+  ws->import(*res);
+
 }
 
 TH2F* Fitter::getSignalRegionHistogram(const TH2F& hist,const char* name) {
@@ -192,6 +258,7 @@ TH2F* Fitter::getSignalRegionHistogram(const TH2F& hist,const char* name) {
   RooRealVar sigregion("signal_region","",-0.5,nXbins*nYbins-0.5);
   sigregion.setBins(nXbins*nYbins);
 
+  RooDataHist norm_rdh( Form("%s_norm_hist",name),"",sigregion);
   RooDataHist rdh( Form("%s_hist",name),"",sigregion);
 
   for(int iX=0;iX<nXbins;iX++) {
@@ -199,14 +266,21 @@ TH2F* Fitter::getSignalRegionHistogram(const TH2F& hist,const char* name) {
     
       sigregion.setVal( iX*4+iY );
       rdh.set(sigregion,signalHist->GetBinContent(iX+1,iY+1));
+      if( (iX==0 && iY < 3) ||
+	  (iX==1 && iY < 2) ||
+	  (iX==2 && iY < 1) )  {
+	norm_rdh.set(sigregion,signalHist->GetBinContent(iX+1,iY+1));
+      }      
     }
   }
 
+  RooHistPdf norm_rhp( Form("%s_norm_pdf",name), "", sigregion, norm_rdh);
   RooHistPdf rhp( Form("%s_pdf",name), "", sigregion, rdh);
 
 
   ws->import(sigregion);
   ws->import(rhp);
+  ws->import(norm_rhp);
 
   return signalHist;
 
@@ -274,52 +348,85 @@ TH2F* Fitter::build_histogram(TString name,int catIndex,region reg,float weight)
 
 void Fitter::saveAll() {
   outputFile->cd();
-  ws->Write();
   
   for(auto& it: save_histograms) {
     it.second->Write();
-    getSignalRegionHistogram(*it.second,it.first+"_SigRegions")->Write();
   }
+  for(auto& it: save_SigRegion_histograms) {
+    it.second->Write();
+  }
+  
+  ws->Write();
+
 }
 
-void Fitter::defineCats() {
-  cats.push_back("SingleMu");
-  cats.push_back("SingleEle");
-  cats.push_back("Hadronic_EBEB");
-
-  per_cat_fit_ranges.push_back({117.,121.,123.5,127.5,129.,133.});
-  per_cat_fit_ranges.push_back({117.,121.,123.5,127.5,129.,133.});
-  per_cat_fit_ranges.push_back({117.,121.,123.5,127.5,129.,133.});
-
-  sideband_integrals.resize(cats.size());
-  signal_integrals.resize(cats.size());
-  sideband_to_signal_scale_factors.resize(cats.size());
-  setupCategory("data");
+void Fitter::setupCats() {
+  if(roocat) delete roocat;
+  roocat = new RooCategory("evtcat","evtcat");
+  //for(auto& it: cats) roocat.defineCategory(it);
+  
+  if( ws->data("data") ) {
+    setupCategory("data",&cats);
+  }else{
+    processData=false;
+  }
   for(auto& it: sm_higgs_names) {
     setupCategory(it);
   }
   for(auto& it: sms_names) {
     setupCategory(it);
   }
+  //cats.push_back("Combined");
+
+  sideband_integrals.resize(cats.size());
+  signal_integrals.resize(cats.size());
+  sideband_to_signal_scale_factors.resize(cats.size());
+
 }
 
-void Fitter::setupCategory(TString name) {
-  {
-    RooDataSet *data = (RooDataSet*)ws->data(name);
-    data = (RooDataSet*)data->reduce("mu1_pt > 20.");
-    ws->import(*data,RooFit::Rename(Form("%s_SingleMu",name.Data())) );
+void Fitter::setupCategory(TString name,std::vector<TString>* catNames) {
+  std::map<std::string,RooDataSet*> sets ;
+
+  RooDataSet *data = (RooDataSet*)ws->data(name);
+  for(auto& it: selectionMap) {
+    RooDataSet* cat_data = (RooDataSet*)data->reduce(it.second.c_str());
+    ws->import(*cat_data,RooFit::Rename(Form("%s_%s",name.Data(),it.first.c_str())) );
+    if(catNames)catNames->push_back(it.first.c_str());
+    sets[it.first]=cat_data;
   }
+
+  RooDataSet comb(name+"_Combined","",*data->get(0),RooFit::Index(*roocat),RooFit::Import(sets));
+  ws->import(comb);
+}
+
+
+
+RooSimultaneous* Fitter::buildSimultaneousPdf(TString name) {
+  RooSimultaneous *sim = new RooSimultaneous( Form(name.Data(),"Simultaneous"), "", *roocat);
   
-  {
-    RooDataSet *data = (RooDataSet*)ws->data(name);
-    data = (RooDataSet*)data->reduce("mu1_pt <= 20. && ele1_pt > 20.");
-    ws->import(*data,RooFit::Rename(Form("%s_SingleEle",name.Data())) );
-  }
+  for(int catIndex=0;catIndex < cats.size();catIndex++) {
+    std::string catname = cats.at(catIndex).Data();
 
-  {
-    RooDataSet *data = (RooDataSet*)ws->data(name);
-    data = (RooDataSet*)data->reduce("mu1_pt <= 20. && ele1_pt <=20.");
-    ws->import(*data,RooFit::Rename(Form("%s_Hadronic_EBEB",name.Data())) );
+    RooAbsPdf *pdf = ws->pdf( Form(name.Data(),catname.c_str()) );
+    sim->addPdf(*pdf,catname.c_str());
   }
+  return sim;
 
+}
+
+void Fitter::defineCats() {
+  per_cat_fit_ranges.push_back({110.,120.,  124.62-1.352*2,124.62+1.352*2, 130.,140.});
+  per_cat_fit_ranges.push_back({110.,120.,  124.46-1.488*2,124.46+1.488*2, 130.,140.});
+  per_cat_fit_ranges.push_back({110.,120.,  124.72-1.289*2,124.72+1.289*2, 130.,140.});
+  per_cat_fit_ranges.push_back({110.,120.,  125.07-1.896*2,125.07+1.896*2, 130.,140.});
+
+  selectionMap = {
+    {"SingleMu","mu1_pt > 20"},
+    {"SingleEle","mu1_pt <= 20. && ele1_pt > 20."},
+    {"Hadronic_EBEB","mu1_pt <= 20. && ele1_pt <=20. && (abs(pho1_eta)<1.48 && abs(pho2_eta)<1.48)"},
+    {"Hadronic_NEBEB","mu1_pt <= 20. && ele1_pt <=20. && !(abs(pho1_eta)<1.48 && abs(pho2_eta)<1.48)"}
+  };
+
+
+  assert(per_cat_fit_ranges.size() == selectionMap.size() && "fit ranges and selection map incompatible");
 }
